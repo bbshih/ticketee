93a6e8c9be2a21ab4a8ac8a106cc515726870ec9
Billy Shih <me@billyshih.com>
2013-05-09 11:04:12 -0700

    removed redundant ticketee folder

--- ticketee/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Ticketee::Application.config.secret_token = '7168fa3ad74d88388d176d554869403b22c4bc4a122e752b52b4bcb109345364bd4b99b2c3369c8da71466c541ad3511ca0beaf08160493cac30d43363b0357f'
