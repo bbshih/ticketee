7833a3c13c4f71dceaa6615eb2a2d6689c42cb2d
Billy Shih <me@billyshih.com>
2013-05-14 10:41:39 -0500

    Added description validation and cleaned up unused specs

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,5 @@ class Ticket < ActiveRecord::Base
   belongs_to :project
   attr_accessible :description, :title
   validates :title, :description, :presence => true
+  validates :description, :length => { :minimum => 10 }
 end
