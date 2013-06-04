e637d534af26309ea191ef3f0d6bf337deded3ff
Billy Shih <me@billyshih.com>
2013-06-04 11:54:35 -0700

    Configured mailer for production env

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Ticketee::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['qzjzrpdv'],
+  :password       => ENV['app16103998@heroku.com'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
